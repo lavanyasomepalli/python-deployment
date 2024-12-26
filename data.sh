d3247ed94466ec53ca034e707e0b8bfcce3122ad
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-26 16:50:42 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/lavanyasomepalli/InNews.git
-sudo mv InNews/ /home/ec2-user/
-cd /home/ec2-user/InNews
+sudo git clone https://github.com/lavanyasomepalli/Agri.git
+sudo mv Agri/ /home/ec2-user/
+cd /home/ec2-user/Agri
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
