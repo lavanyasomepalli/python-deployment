9e8227aa7c942ab6626c9e16e4b59069fa1d91d8
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-26 16:41:03 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/lavanyasomepalli/flight-perdiction.git
-sudo mv flight-perdiction/ /home/ec2-user/
-cd /home/ec2-user/flight-perdiction
+sudo git clone https://github.com/lavanyasomepalli/USA-housing.git
+sudo mv USA-housing/ /home/ec2-user/
+cd /home/ec2-user/USA-housing
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
